394ebbf9463c90cb0e1a89f3e5e211e43df39de7
christopher.fernandes <christyabcd.05@gmail.com>
2023-01-16 12:27:21 +0530

    Implemented table view

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,59 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {HttpClientModule} from '@angular/common/http';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {MatNativeDateModule} from '@angular/material/core';
+import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import "@angular/compiler";
+import {MAT_FORM_FIELD_DEFAULT_OPTIONS} from '@angular/material/form-field';
 
 import { AppComponent } from './app.component';
 import { TreeDiagramModule } from '../../projects/ng-tree-diagram/src/ng-tree-diagram';
 import { TreeV2Module } from 'projects/tree-v2/src/public-api';
+import { RouterModule } from '@angular/router';
+import { TableViewComponent } from './table-view/table-view.component';
+import { DisableControlDirective } from './table-view/disable-form';
+import { MaterialModule } from './material.module';
 
+
+const routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    component: AppComponent,
+  },
+  {
+    path: 'table',
+    pathMatch: 'full',
+    component: TableViewComponent,
+  },
+]
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, TreeDiagramModule, TreeV2Module],
-  providers: [],
+  declarations: [AppComponent, TableViewComponent, DisableControlDirective],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    HttpClientModule,
+    MatNativeDateModule,
+    ReactiveFormsModule,
+    MaterialModule,
+    TreeDiagramModule,
+    TreeV2Module,
+    RouterModule.forRoot(routes, {
+      anchorScrolling: 'enabled',
+      scrollPositionRestoration: 'top',
+      urlUpdateStrategy: 'eager',
+      onSameUrlNavigation: 'reload',
+    }),
+  ],
+  providers: [
+    { provide: MAT_FORM_FIELD_DEFAULT_OPTIONS, useValue: { appearance: 'fill' } },
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+platformBrowserDynamic().bootstrapModule(AppModule)
+  .catch(err => console.error(err));
